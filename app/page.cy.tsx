bb2fc4111fb4f03c6f65974cf112ce912674d95a
Lucas Christian <110307926+Lucas-Christian@users.noreply.github.com>
2023-08-23 01:00:38 +0000

    build: eslint config

--- app/page.cy.tsx
+++ app/page.cy.tsx
@@ -4,8 +4,8 @@ import React from "react";
 describe("<HomePage />", () => {
   it("It is expected to assemble and render the content.", () => {
     cy.mount(<HomePage />);
-    cy.get('p').contains("Teste");
-  })
+    cy.get("p").contains("Teste");
+  });
 });
 
 export {};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import styles from "./page.module.css";
+import React from "react";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <p>Teste</p>
     </main>
-  )
+  );
 }
