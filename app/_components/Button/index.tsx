98f9bf71afbc76889f7637f492f34ad2f7738364
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-09-12 09:45:53 -0300

    build: removed heroicons and packages updated

--- app/_components/Button/index.tsx
+++ app/_components/Button/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-};
+}
 
 export function Button({ children, ...props }: ButtonProps) {
   return (
--- app/_components/Icon/Icon.cy.tsx
+++ app/_components/Icon/Icon.cy.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 describe("<Button />", () => {
   it("It is expected to render all Icons.", () => {
-    for(let icon in icons) {
+    for(const icon in icons) {
       cy.mount(<Icon icon={icon as keyof typeof icons} id="testingIcon" />);
       cy.get("#testingIcon").should("have.css", "color", "rgb(255, 255, 255)");
     }
