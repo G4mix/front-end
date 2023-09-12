c074866fb104d8b64e0da8790639b89abefa5470
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-09-12 08:41:50 -0300

    test: adição de testes para renderização do componente Icon.

--- app/_components/Icon/index.tsx
+++ app/_components/Icon/index.tsx
@@ -1,23 +1,14 @@
 import { FontAwesomeIcon, type FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
-import { faGoogle, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faUser, faLock, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { icons } from "@constants/icons";
 import styles from "./Icon.module.css";
 import React from "react";
 
 type IconProps = {
+  className?: FontAwesomeIconProps["className"];
   icon: "google" | "github" | "linkedin" | "user" | "check" | "lock";
 }
 
 export function Icon({ icon, ...props }: Omit<FontAwesomeIconProps, "icon"> & IconProps) {
-  const icons = {
-    "google": faGoogle,
-    "github": faGithub,
-    "linkedin": faLinkedin,
-    "user": faUser,
-    "check": faCheck,
-    "lock": faLock
-  };
   const IconToRender = icons[icon as keyof typeof icons];
-
-  return <FontAwesomeIcon {...props} className={styles.icon} icon={IconToRender} />;
+  return <FontAwesomeIcon {...props} icon={IconToRender} className={styles.icon} />;
 }
---
+++ app/_constants/icons.ts
@@ -0,0 +1,11 @@
+import { faGoogle, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faUser, faLock, faCheck } from "@fortawesome/free-solid-svg-icons";
+
+export const icons = {
+  "google": faGoogle,
+  "github": faGithub,
+  "linkedin": faLinkedin,
+  "user": faUser,
+  "check": faCheck,
+  "lock": faLock
+};
