96d253dcf07ca9e15c1868b782579404a07a4f81
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-10-26 17:46:04 -0300

    fit: fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,6 @@ export function middleware(request: NextRequest) {
 
   const authPaths = ["/auth/signup", "/auth/signin"];
   const hasCookies = request.cookies.get("accessToken") && request.cookies.get("refreshToken");
-  
   if (!authPaths.includes(pathname) && !hasCookies) {
     return NextResponse.redirect(new URL("/auth/signin", request.url));
   } else if (authPaths.includes(pathname) && hasCookies) {
@@ -17,5 +16,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/"
+  matcher: "/(.*)"
 }
