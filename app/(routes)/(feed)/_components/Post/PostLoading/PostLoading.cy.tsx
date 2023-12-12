12a63bfa00991582d392641bee9be5eeac62820b
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-12-12 07:23:14 -0300

    fit: fix in post loading test

--- app/(routes)/(feed)/_components/Post/PostLoading/PostLoading.cy.tsx
+++ app/(routes)/(feed)/_components/Post/PostLoading/PostLoading.cy.tsx
@@ -5,10 +5,11 @@ import React from "react";
 describe("<PostLoading />", () => {
   it("It is expected to render the loading skeleton.", () => {
     cy.mount(
-      <div className={styles.posts}>
+      <div className={styles.posts} id="posts">
         <PostLoading />
       </div>
     );
+    cy.get("#posts").should("be.visible");
   });
 });
 
